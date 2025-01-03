7ab4ae27c01c3ad80940d327d29ef092b086e23d
Jasper Kooij <jasperkooij@gmail.com>
2025-01-03 07:33:47 -0600

    adding Builder and Daisy UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import { isPreviewing, Content } from '@builder.io/sdk-svelte';
+
+	const apiKey = import.meta.env.VITE_PUBLIC_BUILDER_API_KEY;
+	const model = 'page';
+
+	// this data comes from the function in `+page.server.js`, which runs on the server only
+	export let data;
+
+	// show unpublished content when in preview mode.
+	const canShowContent = data.content || isPreviewing();
+</script>
+
+<main>
+	<!-- TO DO: Add your Public API Key in <Content/>-->
+	{#if canShowContent}
+		<Content {model} content={data.content} {apiKey} />
+	{:else}
+		Content Not Found
+	{/if}
+</main>
